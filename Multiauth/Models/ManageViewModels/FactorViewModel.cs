61760d988d29eb7ae8a983e559f2bca06668aaa3
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2017-07-13 12:58:35 +0530

    Initial Commit
    This commit has the necessary functionaltiy to manage users. It has the
    ability to Register and login  a new user. It provides social connect
    functionlaity using Facebook and also uses sendgrid to send emails to
    users.

---
+++ Multiauth/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Multiauth.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
